04c65b7fb668b53322e673ae034539c09563e539
ruivergani <ruiverganineto@gmail.com>
2023-01-08 21:58:06 +0000

    Offline Data

--- public/js/scripts/main.js
+++ public/js/scripts/main.js
@@ -1,3 +1,5 @@
+  const recipes = document.querySelector('.s-recipes');
+  
   document.addEventListener('DOMContentLoaded', function() { // wait for DOM content to load
     // Nav Menu
     const menus = document.querySelectorAll('.side-menu');
@@ -24,4 +26,58 @@
 
 
   // Database Configuration
-  db.collection('recipes')
+
+  // Offline Data
+  db.enablePersistence()
+    .catch(err => {
+      if(err.code == 'failed-precondition'){
+        // probably multiple tabs open at once
+        console.log('persistence failed');
+      }
+      else if(err.code == 'unimplemented'){
+        // lack of browser support
+        console.log('Persistence is not available');
+      }
+    });
+
+  // Realtime listener
+  db.collection('recipes').onSnapshot((snapshot) => {
+    console.log(snapshot.docChanges());
+    snapshot.docChanges().forEach(change => {
+      //console.log(change, change.doc.data(), change.doc.id);
+      if(change.type === 'added'){
+        // add document data to the web page
+        renderRecipe(change.doc.data(), change.doc.id);
+      }
+      if(change.type === 'removed'){
+        // remove the document data from web page
+      }
+    })
+  });
+
+  // Render Recipe function
+  const renderRecipe = (data, id) => {
+    // Create template HTML with data
+    const html = `
+        <!-- Card Panel for each item -->
+        <div class="card-panel recipe white row" data-id="${id}">
+          <!-- Use suitable and optimised media of the correct format -->
+          <picture>
+            <!-- Min Width: 200px-->
+            <source srcset="./img/dish.webp" media="(min-width: 200px)" type="image/webp">
+            <!-- Fallback image -->
+            <img src="./img/fallback_dish.png" alt="Noodles dish" title="Noodles dish" loading="lazy">
+          </picture>
+          <div class="recipe-details">
+            <div class="recipe-title">${data.title}</div>
+            <div class="recipe-ingredients">${data.ingredients}</div>
+          </div>
+          <div class="recipe-delete">
+            <i class="material-icons" data-id="${id}">delete_outline</i>
+          </div>
+        </div>
+    `;
+    recipes.innerHTML += html;
+  }
+
+ 
--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,6 +1,7 @@
 //set up cache name and files to add to it
-const CACHE_NAME = "my-site-v1";
-const CACHE_URLS = [
+const staticCacheName = 'site-static-v3';
+const dynamicCacheName = 'site-dynamic-v3';
+const assets = [
   "/",
   "index.html",
   "home.html",
@@ -15,10 +16,10 @@ const CACHE_URLS = [
   "css/plugins.css",
   "js/all.js",
   "js/plugins.js",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js",
-  "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap",
-  "https://fonts.gstatic.com/s/materialicons/v139/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+  //"https://fonts.googleapis.com/icon?family=Material+Icons",
+  //"https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js",
+  //"https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap",
+  //"https://fonts.gstatic.com/s/materialicons/v139/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
   "img/dish.webp",
   "img/fallback_dish.png",
   "img/fallback_lasagne.jpg",
@@ -37,82 +38,107 @@ const CACHE_URLS = [
   "img/icons/icon-96x96.png",
 ];
 
-//set up cache and files to add to it
-//...
-
-// Cache size limit function
+// cache size limit function
 const limitCacheSize = (name, size) => {
   caches.open(name).then(cache => {
     cache.keys().then(keys => {
       if(keys.length > size){
-        // call function until not true
         cache.delete(keys[0]).then(limitCacheSize(name, size));
       }
-    })
-  })
+    });
+  });
 };
 
-//add all URLs to cache when installed
-self.addEventListener("install", function (event) {
+// Install Event
+self.addEventListener('install', evt => {
   console.log("Service worker installed");
-  event.waitUntil(
-    //create and open cache
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Cache opened");
-      //add all URLs to cache
-      return cache.addAll(CACHE_URLS);
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("Cache opened (assets)");
+      cache.addAll(assets);
     })
   );
 });
 
-//On activate update the cache with the new version and clean out old caches
-self.addEventListener("activate", function (event) {
+// //add all URLs to cache when installed
+// self.addEventListener("install", function (event) {
+//   console.log("Service worker installed");
+//   event.waitUntil(
+//     //create and open cache
+//     caches.open(CACHE_NAME).then(function (cache) {
+//       console.log("Cache opened");
+//       //add all URLs to cache
+//       return cache.addAll(CACHE_URLS);
+//     })
+//   );
+// });
+
+// Activate Event
+self.addEventListener('activate', evt => {
   console.log('Service worker activated');
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheName.startsWith("my-site-") && CACHE_NAME !== cacheName) {
-            return caches.delete(cacheName);
-          }
-        })
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .map(key => caches.delete(key))
       );
     })
   );
 });
 
-//add all URLs to cache when installed
-//...
+// //On activate update the cache with the new version and clean out old caches
+// self.addEventListener("activate", function (event) {
+//   console.log('Service worker activated');
+//   event.waitUntil(
+//     caches.keys().then(function (cacheNames) {
+//       return Promise.all(
+//         cacheNames.map(function (cacheName) {
+//           if (cacheName.startsWith("my-site-") && CACHE_NAME !== cacheName) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+
+// Fetch Event
+self.addEventListener('fetch', evt => {
+  if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
+    evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 15);
+            return fetchRes;
+          })
+        });
+      }).catch(() => {
+        if(evt.request.url.indexOf('.html') > -1){
+          return caches.match('/pages/fallback.html');
+        }
+      })
+    );
+  }
+});
+
 
 //user has navigated to page - fetch required assets
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      //check whether asset is in cache
-      if (response) {
-        //asset in cache, so return it
-        console.log(`Return ${event.request.url} from cache`);
-        return response;
-      }
-      //asset not in cache so fetch asset from network
-      console.log(`Fetch ${event.request.url} from network`);
-      return fetch(event.request);
-    })
-  );
-});
-
-// On Network Response
-const onNetworkResponse = (event) => {
-  event.respondWith(
-    caches.open("mysite-dynamic").then(async function (cache) {
-      const response = await cache.match(event.request);
-      return (
-        response ||
-        fetch(event.request).then(function (response_1) {
-          cache.put(event.request, response_1.clone());
-          return response_1;
-        })
-      );
-    })
-  );
-};
+// self.addEventListener("fetch", function (event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       //check whether asset is in cache
+//       if (response) {
+//         //asset in cache, so return it
+//         console.log(`Return ${event.request.url} from cache`);
+//         return response;
+//       }
+//       //asset not in cache so fetch asset from network
+//       console.log(`Fetch ${event.request.url} from network`);
+//       return fetch(event.request);
+//     })
+//   );
+// });

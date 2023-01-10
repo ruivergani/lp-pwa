569f1c48745841a8af8ddb13e52ab25cdfec7082
ruivergani <ruiverganineto@gmail.com>
2023-01-10 19:40:33 +0000

    changes service worker

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const recipes = document.querySelector('.s-recipes');
 
+M.AutoInit();
+
 document.addEventListener('DOMContentLoaded', function() { // wait for DOM content to load
   // Nav Menu
   const menus = document.querySelectorAll('.side-menu');
@@ -11,11 +13,14 @@ document.addEventListener('DOMContentLoaded', function() { // wait for DOM conte
 
 // Preloader Effect
 const preloader = document.getElementById('preloader');
-// After window is loaded
-window.onload = () => {
-  // Hide the preloader div
-  preloader.style.display = "none";
+if (preloader){
+  // After window is loaded
+  window.onload = () => {
+    // Hide the preloader div
+    preloader.style.display = "none";
+  }
 }
+
 // Initialize the elements
 $(document).ready(function(){
   // Select Materialize
@@ -47,74 +52,87 @@ db.enablePersistence()
   });
 
 // Realtime listener
-db.collection('recipes').onSnapshot((snapshot) => {
-  console.log(snapshot.docChanges());
-  snapshot.docChanges().forEach(change => {
-    //console.log(change, change.doc.data(), change.doc.id);
-    if(change.type === 'added'){
-      // add document data to the web page
-      renderRecipe(change.doc.data(), change.doc.id);
-    }
-    if(change.type === 'removed'){
-      // remove the document data from web page
-      removeRecipe(change.doc.id); // id of the doc removed
-    }
-  })
-});
-
-// Render Recipe function
-const renderRecipe = (data, id) => {
-  // Create template HTML with data
-  const html = `
-      <!-- Card Panel for each item -->
-      <div class="card-panel recipe white row" data-id="${id}">
-        <!-- Use suitable and optimised media of the correct format -->
-        <picture>
-          <!-- Min Width: 200px-->
-          <source srcset="./img/dish.svg" media="(min-width: 200px)" type="image/webp">
-          <!-- Fallback image -->
-          <img src="./img/fallback_dish.png" alt="Noodles dish" title="Noodles dish" loading="lazy">
-        </picture>
-        <div class="recipe-details">
-          <div class="recipe-title">${data.title}</div>
-          <div class="recipe-ingredients">${data.ingredients}</div>
-        </div>
-        <div class="recipe-delete">
-          <i class="material-icons" data-id="${id}">delete_outline</i>
-        </div>
-      </div>
-  `;
-  recipes.innerHTML += html;
+if(recipes){
+  db.collection('recipes').onSnapshot((snapshot) => {
+    console.log(snapshot.docChanges());
+    snapshot.docChanges().forEach(change => {
+      //console.log(change, change.doc.data(), change.doc.id);
+      if(change.type === 'added'){
+        // add document data to the web page
+        renderRecipe(change.doc.data(), change.doc.id);
+      }
+      if(change.type === 'removed'){
+        // remove the document data from web page
+        removeRecipe(change.doc.id); // id of the doc removed
+      }
+    })
+  });
 }
 
-// Add New Recipe
-const form = document.querySelector('.add-recipe');
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-
-  const recipe = {
-    title: form.title.value,
-    ingredients: form.ingredients.value
-  };
-
-  db.collection('recipes').add(recipe)
-    .catch(err => console.error(err));
-  
-  form.title.value = '';
-  form.ingredients.value = '';
-});
-
-// Delete Recipe
-const recipeContainer = document.querySelector('.s-recipes');
-recipeContainer.addEventListener('click', evt => {
-  if(evt.target.tagName === 'I'){
-    const id =  evt.target.getAttribute('data-id'); //  get the id attribute
-    db.collection('recipes').doc(id).delete();
+// Check if the content exists before using eventListeners
+if(recipes){
+  // Render Recipe function
+  const renderRecipe = (data, id) => {
+    // Create template HTML with data
+    const html = `
+        <!-- Card Panel for each item -->
+        <div class="card-panel recipe white row" data-id="${id}">
+          <!-- Use suitable and optimised media of the correct format -->
+          <picture>
+            <!-- Min Width: 200px-->
+            <source srcset="./img/dish.svg" media="(min-width: 200px)" type="image/webp">
+            <!-- Fallback image -->
+            <img src="./img/dish.png" alt="dish picture" title="dish picture" loading="lazy">
+          </picture>
+          <div class="recipe-details">
+            <div class="recipe-title">${data.title}</div>
+            <div class="recipe-ingredients">${data.ingredients}</div>
+          </div>
+          <div class="recipe-delete">
+            <i class="material-icons" data-id="${id}">delete_outline</i>
+          </div>
+        </div>
+    `;
+    recipes.innerHTML += html;
   }
-});
-
-// Remove Recipe function
-const removeRecipe = (id) => {
-  const recipe = document.querySelector(`.recipe[data-id=${id}]`);
-  recipe.remove();
 }
+
+if(recipes){
+  // Add New Recipe
+  const formRecipe = document.querySelector('.add-recipe');
+  if(formRecipe){
+    formRecipe.addEventListener('submit', evt => {
+      evt.preventDefault();
+    
+      const recipe = {
+        title: form.title.value,
+        ingredients: form.ingredients.value
+      };
+    
+      db.collection('recipes').add(recipe)
+        .catch(err => console.error(err));
+      
+      form.title.value = '';
+      form.ingredients.value = '';
+    });
+  }
+
+  // Delete Recipe
+  const recipeContainer = document.querySelector('.s-recipes');
+  if(recipeContainer){
+    recipeContainer.addEventListener('click', evt => {
+      if(evt.target.tagName === 'I'){
+        const id =  evt.target.getAttribute('data-id'); //  get the id attribute
+        db.collection('recipes').doc(id).delete();
+      }
+    });
+  }
+
+  // Remove Recipe function
+  const removeRecipe = (id) => {
+    const recipe = document.querySelector(`.recipe[data-id=${id}]`);
+    recipe.remove();
+  }
+}
+
+

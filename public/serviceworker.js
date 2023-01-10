f610cc269445a5626b18f0f3bcf33b2f7d9836c6
ruivergani <ruiverganineto@gmail.com>
2023-01-10 12:00:30 +0000

    changes folder

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -60,18 +60,6 @@ self.addEventListener('install', evt => {
   );
 });
 
-// //add all URLs to cache when installed
-// self.addEventListener("install", function (event) {
-//   console.log("Service worker installed");
-//   event.waitUntil(
-//     //create and open cache
-//     caches.open(CACHE_NAME).then(function (cache) {
-//       console.log("Cache opened");
-//       //add all URLs to cache
-//       return cache.addAll(CACHE_URLS);
-//     })
-//   );
-// });
 
 // Activate Event
 self.addEventListener('activate', evt => {
@@ -86,22 +74,6 @@ self.addEventListener('activate', evt => {
     })
   );
 });
-
-// //On activate update the cache with the new version and clean out old caches
-// self.addEventListener("activate", function (event) {
-//   console.log('Service worker activated');
-//   event.waitUntil(
-//     caches.keys().then(function (cacheNames) {
-//       return Promise.all(
-//         cacheNames.map(function (cacheName) {
-//           if (cacheName.startsWith("my-site-") && CACHE_NAME !== cacheName) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
 
 // Fetch Event
 self.addEventListener('fetch', evt => {
@@ -128,21 +100,3 @@ self.addEventListener('fetch', evt => {
     );
   }
 });
-
-
-//user has navigated to page - fetch required assets
-// self.addEventListener("fetch", function (event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function (response) {
-//       //check whether asset is in cache
-//       if (response) {
-//         //asset in cache, so return it
-//         console.log(`Return ${event.request.url} from cache`);
-//         return response;
-//       }
-//       //asset not in cache so fetch asset from network
-//       console.log(`Fetch ${event.request.url} from network`);
-//       return fetch(event.request);
-//     })
-//   );
-// });
